f1896ae7b45fef05e2e9cdd698cfe50e84202f7f
markpassawat <markpassawat.dev@gmail.com>
2024-02-14 14:12:05 +0700

    added greetingPrefix function to support multiple languages

--- Fundamentals/hello, world/hello.go
+++ Fundamentals/hello, world/hello.go
@@ -1,10 +1,38 @@
 package main
 
-const englishHelloPrefix = "Hello, "
+import "strings"
 
-func Hello(name string) string {
+/*
+Write a test
+Make the compiler pass
+Run the test, see that it fails and check the error message is meaningful
+Write enough code to make the test pass
+Refactor
+*/
+const (
+	spanish = "spanish"
+	thai    = "thai"
+
+	spanishHelloPrefix = "Hola, "
+	englishHelloPrefix = "Hello, "
+	thaiHelloPrefix    = "สวัสดี, "
+)
+
+func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
 	}
-	return englishHelloPrefix + name
+	return greetingPrefix(language) + name
+}
+
+func greetingPrefix(language string) (prefix string) {
+	switch strings.ToLower(language) {
+	case spanish:
+		prefix = spanishHelloPrefix
+	case thai:
+		prefix = thaiHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return
 }
--- Fundamentals/hello, world/hello_test.go
+++ Fundamentals/hello, world/hello_test.go
@@ -10,20 +10,29 @@ test function takes one argument only t *testing.T with import "testing"
 func TestHello(t *testing.T) {
 	// `go test -v` prints the full output
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Mark")
+		got := Hello("Mark", "English")
 		want := "Hello, Mark"
-
 		assertCorrectMessage(t, got, want)
 	})
 	t.Run("empty string defaults to 'world'", func(t *testing.T) {
-		got := Hello("")
-		want := "Hello, Word"
+		got := Hello("", "English")
+		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("in Thai", func(t *testing.T) {
+		got := Hello("Elodie", "Thai")
+		want := "สวัสดี, Elodie"
 		assertCorrectMessage(t, got, want)
 	})
 }
 
 func assertCorrectMessage(t testing.TB, got string, want string) {
-	t.Helper() // comment this out and modified sub-test to fail, it help developer to spot bug
+	t.Helper() // comment this out and modified sub-test to fail, it help developer to spot a line of bugged code
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
