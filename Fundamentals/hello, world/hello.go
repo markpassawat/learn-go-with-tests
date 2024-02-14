b9e97462d63593d11940114f1edc9b71477d17df
markpassawat <markpassawat.dev@gmail.com>
2024-02-14 12:09:29 +0700

    added hello world testcase

--- Fundamentals/hello, world/hello.go
+++ Fundamentals/hello, world/hello.go
@@ -1,5 +1,10 @@
 package main
 
-func Hello() string {
-	return "Hello, world"
+const englishHelloPrefix = "Hello, "
+
+func Hello(name string) string {
+	if name == "" {
+		name = "World"
+	}
+	return englishHelloPrefix + name
 }
--- Fundamentals/hello, world/hello_test.go
+++ Fundamentals/hello, world/hello_test.go
@@ -8,9 +8,22 @@ test function must start with the word `Test`
 test function takes one argument only t *testing.T with import "testing"
 */
 func TestHello(t *testing.T) {
-	got := Hello()
-	want := "Hello, world"
+	// `go test -v` prints the full output
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Mark")
+		want := "Hello, Mark"
 
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("empty string defaults to 'world'", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, Word"
+		assertCorrectMessage(t, got, want)
+	})
+}
+
+func assertCorrectMessage(t testing.TB, got string, want string) {
+	t.Helper() // comment this out and modified sub-test to fail, it help developer to spot bug
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
