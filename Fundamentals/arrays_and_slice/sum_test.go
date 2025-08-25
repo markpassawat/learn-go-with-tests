1cd288ef9571953993fe014bacc108f64083cf89
markpassawat <markpassawat.dev@gmail.com>
2025-08-25 11:50:23 +0700

    feat: TestSum unit-test

---
+++ Fundamentals/arrays_and_slice/sum_test.go
@@ -0,0 +1,19 @@
+package arraysandslice
+
+import "testing"
+
+func TestSum(t *testing.T) {
+
+	/*
+		[N]type{value1, value2, ..., valueN} e.g. numbers := [5]int{1, 2, 3, 4, 5}
+		[...]type{value1, value2, ..., valueN} e.g. numbers := [...]int{1, 2, 3, 4, 5}
+	*/
+	numbers := []int{1, 2, 3, 4, 5}
+
+	got := Sum(numbers)
+	want := 15
+
+	if got != want {
+		t.Errorf("got %d want %d given, %v", got, want, numbers)
+	}
+}
