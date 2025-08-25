2279a2163cf530c13fb0c592d6bd1877d653c94c
markpassawat <markpassawat.dev@gmail.com>
2025-08-25 13:20:48 +0700

    fix: update Sum function to accept a slice of integers and adjust tests accordingly

--- Fundamentals/arrays_and_slice/sum_test.go
+++ Fundamentals/arrays_and_slice/sum_test.go
@@ -8,12 +8,24 @@ func TestSum(t *testing.T) {
 		[N]type{value1, value2, ..., valueN} e.g. numbers := [5]int{1, 2, 3, 4, 5}
 		[...]type{value1, value2, ..., valueN} e.g. numbers := [...]int{1, 2, 3, 4, 5}
 	*/
-	numbers := [5]int{1, 2, 3, 4, 5}
+	t.Run("collection of 5 numbers", func(t *testing.T) {
+		numbers := []int{1, 2, 3, 4, 5}
+		got := Sum(numbers)
+		expected := 15
 
-	got := Sum(numbers)
-	want := 15
+		if got != expected {
+			t.Errorf("got %d want %d given %v", got, expected, numbers)
+		}
+	})
 
-	if got != want {
-		t.Errorf("got %d want %d given, %v", got, want, numbers)
-	}
+	t.Run("collection of 5 other numbers", func(t *testing.T) {
+		numbers := []int{1, 2, 3}
+
+		got := Sum(numbers)
+		expected := 6
+
+		if got != expected {
+			t.Errorf("got %d want %d given %v", got, expected, numbers)
+		}
+	})
 }
